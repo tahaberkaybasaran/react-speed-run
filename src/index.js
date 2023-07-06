23fb84e67465e165f746dee46fd4fdc295623466
Trigonometri1 <tahaberkaybasaran@gmail.com>
2023-07-06 18:31:21 +0300

    first commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,36 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const Greeting = () => {
+  return (
+    <>
+      <React.StrictMode>
+        <h2>First Reeact Component</h2>
+        <Book />
+        <Person />
+        <Message />
+      </React.StrictMode>
+    </>
+  );
+};
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const Book = () => {
+  return (
+    <>
+      <input type="text" name="" id="" />
+      <hr />
+      <h3>hello</h3>
+    </>
+  );
+};
+
+const Person = () => {
+  return <div>hi im a person</div>;
+};
+const Message = () => {
+  return <div>hi im a Message from another space</div>;
+};
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(<Greeting />);
