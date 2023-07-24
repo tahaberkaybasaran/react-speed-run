477832a0ef6eefaa2f3a71166f3e0fb3f50704e5
Trigonometri1 <tahaberkaybasaran@gmail.com>
2023-07-24 16:35:54 +0300

    The Spread Operator

--- src/index.js
+++ src/index.js
@@ -9,30 +9,16 @@ const BookList = () => {
     <>
       <section className="booklist">
         {Books.map((book) => {
-          const { image, title, author } = book;
-          return <Book book={book} />;
+          // const { image, title, author } = book;
+          return <Book key={book.id} {...book} />;
         })}
       </section>
     </>
   );
 };
 
-// const Book = (props) => {
-//   const { image, title, author } = props.book;
-//   // const { img, title, author } = props;
-//   return (
-//     <>
-//       <article className="book">
-//         <img src={image} alt={title} />
-//         <h2>{title}</h2>
-//         <h3>{author}</h3>
-//       </article>
-//     </>
-//   );
-// };
-
-const Book = ({ book: { image, title, author } }) => {
-  // const { img, title, author } = props;
+const Book = (props) => {
+  const { image, title, author } = props;
   return (
     <>
       <article className="book">
