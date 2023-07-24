822679932c15c82894e4bad754582551b3ed8115
Trigonometri1 <tahaberkaybasaran@gmail.com>
2023-07-24 18:11:56 +0300

    events onClick etc

--- src/index.js
+++ src/index.js
@@ -8,12 +8,44 @@ const BookList = () => {
   return (
     <>
       <section className="booklist">
+        <EventExample />
         {Books.map((book) => {
           // const { image, title, author } = book;
           return <Book key={book.id} {...book} />;
         })}
       </section>
     </>
+  );
+};
+
+const EventExample = () => {
+  const handleFormInput = (event) => {
+    if (event.target.value.length > 10) {
+      alert("You have reached to 10 character limit");
+    }
+  };
+  const handleButtonClick = () => {
+    alert(`Button Has Clicked`);
+  };
+
+  const handleFormSubmit = (e) => {
+    e.preventDefault();
+    console.log("Form Submitted");
+  };
+  return (
+    <section>
+      <form onSubmit={handleFormSubmit}>
+        <h2>Typical Form</h2>
+        <input
+          type="text"
+          name="example"
+          style={{ margin: "1rem 0", width: "10rem", height: "1.5rem" }}
+          onChange={handleFormInput}
+        />
+        <button type="submit">submit</button>
+      </form>
+      <button onClick={handleButtonClick}>click</button>
+    </section>
   );
 };
 
