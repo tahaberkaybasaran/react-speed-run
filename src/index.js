23348310cab109240c349cee548baa6de58627d4
Trigonometri1 <tahaberkaybasaran@gmail.com>
2023-07-27 17:59:01 +0300

    using props for every specific components

--- src/index.js
+++ src/index.js
@@ -8,7 +8,6 @@ const BookList = () => {
   return (
     <>
       <section className="booklist">
-        <EventExample />
         {Books.map((book) => {
           // const { image, title, author } = book;
           return <Book key={book.id} {...book} />;
@@ -18,39 +17,27 @@ const BookList = () => {
   );
 };
 
-const EventExample = () => {
-  const handleFormSubmit = (e) => {
-    e.preventDefault();
-    console.log("Form Submitted");
-  };
-  return (
-    <section>
-      <form onSubmit={handleFormSubmit}>
-        <h2>Typical Form</h2>
-        <input
-          type="text"
-          name="example"
-          style={{ margin: "1rem 0", width: "10rem", height: "1.5rem" }}
-          onChange={(event) => {
-            if (event.target.value.length > 10) {
-              alert("You have reached to 10 character limit");
-            }
-          }}
-        />
-        <button type="submit">submit</button>
-      </form>
-      <button onClick={() => alert("Button Has Clicked")}>click</button>
-    </section>
-  );
-};
-
 const Book = (props) => {
   const { image, title, author } = props;
+  const displayTitle = () => {
+    console.log(`display the ${title} test`);
+  };
   return (
     <>
       <article className="book">
         <img src={image} alt={title} />
         <h2>{title}</h2>
+        <button
+          onClick={displayTitle}
+          style={{
+            margin: "5px",
+            fontSize: "17px",
+            padding: "5px",
+            borderRadius: "5px",
+          }}
+        >
+          Display Title
+        </button>
         <h3>{author}</h3>
       </article>
     </>
